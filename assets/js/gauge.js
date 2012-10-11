9fc80cce7c6eba0d8ba44a8b56427baf2a9eb961
James Jenner <james.g.jenner@gmail.com>
2012-10-11 21:27:25 +1000

    Initial support for canvas

--- assets/js/gauge.js
+++ assets/js/gauge.js
@@ -9,6 +9,8 @@ var Gauge = function (argmap) {
     } else {
         this.id = this.name + 'Panel';
     }
+    this.canvasId = this.id + '_canvas';
+    
     if('title' in argmap) {
         this.title = argmap.title;
     }
@@ -46,19 +48,51 @@ var Gauge = function (argmap) {
     } else {
         this.foregroundImgClips = false;
     }
+    // make needle a sub type with it's own properties?
     if('needleImg1' in argmap) {
         this.needleImg1 = argmap.needleImg1;
+    }
+    if('needleImg1Rotates' in argmap) {
+        this.needleImg1Rotates = argmap.needleImg1Rotates;
+    } else {
+        this.needleImg1Rotates = true;
+    }
+    if('needleImg1VerticalScroll' in argmap) {
+        this.needleImg1VerticalScroll = argmap.needleImg1VerticalScroll;
+    } else {
+        this.needleImg1VerticalScroll = false;
+    }
+    if('needleImg1VerticalScrollType' in argmap) {
+        this.needleImg1VerticalScrollType = argmap.needleImg1VerticalScrollType;
+    } else {
+        this.needleImg1VerticalScrollType = 'fixed';
     }
     if('needleImg2' in argmap) {
         this.needleImg2 = argmap.needleImg2;
     }
+    if('needleImg2Rotates' in argmap) {
+        this.needleImg2Rotates = argmap.needleImg2Rotates;
+    } else {
+        this.needleImg2Rotates = true;
+    }
     if('needleImg3' in argmap) {
         this.needleImg3 = argmap.needleImg3;
+    }
+    if('needleImg3Rotates' in argmap) {
+        this.needleImg3Rotates = argmap.needleImg3Rotates;
+    } else {
+        this.needleImg3Rotates = true;
     }
     if('icon' in argmap) {
         this.icon = argmap.icon;
     }
     this.enabled = false;
+    
+    if('contentIsCanvas' in argmap) {
+        this.contentIsCanvas = argmap.contentIsCanvas;
+    } else {
+        this.contentIsCanvas = false;
+    }
 }
     
 Gauge.prototype = {
@@ -78,5 +112,8 @@ Gauge.prototype = {
     },
     setNeedle3Image: function(i) {
         this.panelNeedleImg3 = i;
+    },
+    setValue: function(v) {
+        
     },
 };
